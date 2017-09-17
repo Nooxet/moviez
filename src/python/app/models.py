413231b2834430cda1867946659ed5feaa63ffe5
Jonathan Sönnerup <Nooxet@gmail.com>
2017-09-18 00:52:17 +0200

    Added models for movie, genre and actor along with their relationships. Also added test routes to try it

--- src/python/app/models.py
+++ src/python/app/models.py
@@ -1,17 +1,12 @@
 
 from app import db
 
-class Movie(db.Model):
-
-    __tablename__ = 'movies'
+class DBModel():
+    """Superclass for all database models"""
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    rating = db.Column(db.Integer)
-
-    def __init__(self, title, rating):
-        self.title = title
-        self.rating = rating
+    date_created = db.Column(db.DateTime)
+    date_updated = db.Column(db.DateTime)
 
     def save(self):
         db.session.add(self)
@@ -21,5 +16,66 @@ class Movie(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+
+movies_actors = db.Table('movies_actors',
+    db.Column('movies_id', db.Integer, db.ForeignKey('movies.id')),
+    db.Column('actors_id', db.Integer, db.ForeignKey('actors.id'))
+)
+
+movies_genres = db.Table('movies_genres',
+    db.Column('movies_id', db.Integer, db.ForeignKey('movies.id')),
+    db.Column('genres_id', db.Integer, db.ForeignKey('genres.id'))
+)
+
+class Movie(db.Model, DBModel):
+
+    __tablename__ = 'movies'
+
+    # IMDb data
+    imdb_id     = db.Column(db.String(20))
+    title       = db.Column(db.String(255))
+    plot        = db.Column(db.String(2047))
+    year        = db.Column(db.Integer)
+    rating      = db.Column(db.Integer)
+    genres      = db.relationship('Genre', secondary=movies_genres,
+                    backref=db.backref('movies', lazy='dynamic'))
+    actors      = db.relationship('Actor', secondary=movies_actors,
+                    backref=db.backref('movies', lazy='dynamic'))
+
+    # User specified data
+    user_summary    = db.Column(db.String(2047))
+    user_rating     = db.Column(db.Integer)
+    user_seen       = db.Column(db.Integer)
+
+    def __init__(self, title, rating):
+        self.title = title
+        self.rating = rating
+        #self.actor = actor
+
     def __repr__(self):
-        return "Movie: {}".format(self.name)
+        return "Movie: {}".format(self.title)
+
+
+class Genre(db.Model, DBModel):
+
+    __tablename__ = 'genres'
+
+    genre = db.Column(db.String(20))
+
+    def __init__(self, genre):
+        self.genre = genre
+
+    def __repr__(self):
+        return self.genre
+
+class Actor(db.Model, DBModel):
+
+    __tablename__ = 'actors'
+
+    name = db.Column(db.String(255))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
--- src/python/app/routes.py
+++ src/python/app/routes.py
@@ -2,6 +2,29 @@ from app import app, models
 
 @app.route('/')
 def index():
+    # get all actors for the movie
+    m = models.Movie.query.get(1)
+    print (m)
+    print ([i for i in m.actors])
+
+    # get all movies for the actor
+    a = models.Actor.query.get(2)
+    print ("Actor", a)
+    print ([i for i in a.movies])
+    return "Got"
+
+@app.route('/add')
+def add():
+    slj = models.Actor('Samuel L. Jackson')
+    bw = models.Actor('Bruce Willys')
+
     m = models.Movie('Pulp Fiction', 5)
+    m.actors.append(slj)
+    m.actors.append(bw)
     m.save()
-    return "Hello, World"
+
+    mm = models.Movie('Breaking Bad', 5)
+    mm.actors.append(bw)
+    mm.save()
+
+    return "Added!"
