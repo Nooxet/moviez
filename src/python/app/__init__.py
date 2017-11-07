82c6df7f9ab663da996da526161dc0d09c62e945
Jonathan Sönnerup <Nooxet@gmail.com>
2017-11-07 23:01:29 +0100

    added CORS

--- src/python/app/__init__.py
+++ src/python/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+CORS(app)
 db = SQLAlchemy(app)
 
 from app import routes, models
